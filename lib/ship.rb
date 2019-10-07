45f47f3f27405499b33be4443904f9aeebf23da8
msimon42 <msimon@uab.edu>
2019-10-07 17:29:43 -0600

    Create Ship class

---
+++ lib/ship.rb
@@ -0,0 +1,20 @@
+
+
+class Ship
+  attr_reader :name, :length, :health, :sunk
+
+  def initialize(name, length)
+    @name = name
+    @length = length
+    @health = self.length
+    @sunk = false
+  end
+
+  def hit
+    @health -= 1
+
+    if @health == 0
+      @sunk = true
+    end
+  end
+end
