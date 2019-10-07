150c268538331562498c2b6fbf8ccb8ec0a5ccc6
Madelyn Romero <m.romero013@gmail.com>
2019-10-07 17:54:28 -0600

    Add Cell, render method

--- lib/cell.rb
+++ lib/cell.rb
@@ -14,6 +14,18 @@ class Cell
   end
 
   def fire_upon
-    
+    true
+  end
+
+  def render
+    if self.fired_upon == false
+      '.'
+    elsif self.fired_upon == true && self.ship == nil
+      'M'
+    elsif self.fired_upon == true && self.ship == true
+      'H'
+    elsif self.fired_upon == true && self.ship.sunk == true
+      'X'
+    end
   end
 end
