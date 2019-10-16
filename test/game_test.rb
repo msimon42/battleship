81037aacd6581c236dd8bf2b886edb070947e471
msimon42 <msimon@uab.edu>
2019-10-16 15:23:32 -0600

    Add tests for intelligent guess

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,7 +1,21 @@
 require 'minitest/autorun'
+require_relative '../lib/game'
 
-class MyTest < MiniTest::Unit::TestCase
+class MyTest < MiniTest::Test
   def setup
     @game = Game.new
+  end
+
+  def test_guess_rows
+    skip
+    @game.computer_player.record_hit('B2')
+    @game.computer_player.record_hit('B3')
+    assert true, @game.human_player.board.cells.keys.include?(@game.guess(@game.computer_player))
+  end
+
+  def test_guess_cols
+    @game.computer_player.record_hit('B2')
+    @game.computer_player.record_hit('C2')
+    assert true, @game.human_player.board.cells.keys.include?(@game.guess(@game.computer_player))
   end
 end
